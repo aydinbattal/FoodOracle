91f16fb34e4bf935a8b029661a41e4498e9daffd
Aydin Battal <abattal35@gmail.com>
2021-11-22 17:44:44 -0500

    Initial Commit

---
+++ FoodOracle/FoodOracleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodOracleApp.swift
+//  FoodOracle
+//
+//  Created by Aydin Battal on 2021-11-22.
+//
+
+import SwiftUI
+
+@main
+struct FoodOracleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
