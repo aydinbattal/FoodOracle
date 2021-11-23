ba29e694ba655fc1f9409eaaf2e0a71dfe4b21d4
Aydin Battal <abattal35@gmail.com>
2021-11-22 19:45:20 -0500

    Implemented logic for searching. Needs data from db.

---
+++ FoodOracle/Controllers/DBHelper.swift
@@ -0,0 +1,17 @@
+//
+//  DBHelper.swift
+//  FoodOracle
+//
+//  Created by Aydin Battal on 2021-11-22.
+//
+
+import Foundation
+import SwiftUI
+
+class DBHelper : ObservableObject{
+//    @Published var receiptList = [Book]()
+    @Published var receiptList = ["Apple", "Banana", "Peach"]
+    
+}
+
+
