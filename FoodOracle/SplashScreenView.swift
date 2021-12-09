e0f0c5eec84380bbeff7b1d769b9f1183029604a
Karan Patel <pate1228@sheridancollege.ca>
2021-12-08 21:49:30 -0500

    Splash screen

---
+++ FoodOracle/SplashScreenView.swift
@@ -0,0 +1,35 @@
+//
+//  SplashScreenView.swift
+//  FoodOracle
+//
+//  Created by Karan Patel on 2021-12-08.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    @State private var isActive = false
+    let main = ContentView()
+    let color = Color.init("black_1")
+
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .center) {
+                Image("foodoracle")
+                NavigationLink(destination: login,
+                               isActive: $isActive,
+                               label: { EmptyView() })
+            }
+            .background(color)
+            .onAppear(perform: {
+                self.gotoMainScreen(time: 2.5)
+            })
+        }
+    }
+
+    func gotoMainScreen(time: Double) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + Double(time)) {
+            self.isActive = true
+        }
+    }
+}
